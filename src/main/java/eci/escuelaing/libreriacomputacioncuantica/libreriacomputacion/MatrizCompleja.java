d7ce19fbd83c0d7bc7e61c8fe844df7c69f7d723
alejandrordto <masterale2011@gmail.com>
2019-08-20 15:46:47 -0500

    avance matriz

--- src/main/java/eci/escuelaing/libreriacomputacioncuantica/libreriacomputacion/MatrizCompleja.java
+++ src/main/java/eci/escuelaing/libreriacomputacioncuantica/libreriacomputacion/MatrizCompleja.java
@@ -9,37 +9,69 @@ package eci.escuelaing.libreriacomputacioncuantica.libreriacomputacion;
  *
  * @author Alejandro Rodriguez
  */
-    import java.lang.reflect.Array;
-
+import java.lang.reflect.Array;
 
 /**
  *
  * @author ardt1997
  */
 public class MatrizCompleja {
+
     private NumeroComplejo[][] matriz;
     private NumeroComplejo[] vector;
     private boolean esvector;
 
-    public MatrizCompleja( NumeroComplejo[][] matriz) {
+    public MatrizCompleja(NumeroComplejo[][] matriz) {
         this.matriz = matriz;
-        this.esvector= false;
+        this.esvector = false;
     }
-     public MatrizCompleja( NumeroComplejo[]vector) {
-        this.vector = vector;
-        this.esvector=true;
-    }
-    @Override
-    public String toString() {
-        String cadena="";
-        if (this.esvector){
-            cadena="MatrizCompleja{" + "vector=" + vector + '}';
-        }else{
-            cadena="MatrizCompleja{" + "matriz=" + matriz + '}';
-        }
-        return cadena;
-    }
-    
-    
-}
 
+    public MatrizCompleja(NumeroComplejo[] vector) {
+        this.vector = vector;
+        this.esvector = true;
+    }
+
+    public Integer tamaño() {
+        if (esvector) {
+            return vector.length;
+        } else {
+            return matriz.length;
+        }
+
+    }
+
+    public NumeroComplejo[][] getMatriz() {
+        return matriz;
+    }
+
+    public NumeroComplejo[] getVector() {
+        return vector;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        } else if (this.getClass() != obj.getClass()) {
+            return false;
+        }
+        MatrizCompleja mc = (MatrizCompleja) obj;
+        if (this.tamaño() != mc.tamaño()) {
+            return false;
+        }
+        boolean flag = false;
+        if (this.esvector) {
+            NumeroComplejo[] v1= mc.getVector();
+            for (int i = 0; i < vector.length; i++) {
+                //boolean=vector[i].equals(v1[i]);
+            }
+        } else {
+            for (int i = 0; i < matriz.length; i++) {
+                NumeroComplejo[] numeroComplejos = matriz[i];
+
+            }
+        }
+        return true;
+    }
+
+}
