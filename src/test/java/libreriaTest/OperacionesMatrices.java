d3ca9e16d8e4d28364ea00b29d87dd6d7a902d04
alejandrordto <masterale2011@gmail.com>
2019-09-02 20:02:36 -0500

    test

--- src/test/java/libreriaTest/OperacionesMatrices.java
+++ src/test/java/libreriaTest/OperacionesMatrices.java
@@ -112,5 +112,125 @@ public class OperacionesMatrices {
         NumeroComplejo[][] respuesta = operador.multiplicacionEscalarMatriz(lista, nc) ;
         assertEquals(respuesta,esperado);
     }
-    
+    @Test
+    public void MatrizTranspuesta(){
+        
+        NumeroComplejo[][] lista = new NumeroComplejo[2][2];
+        NumeroComplejo[][]  esperado = new NumeroComplejo[2][2];
+        lista[0][0] =new NumeroComplejo(1,0);
+        lista[0][1] =new NumeroComplejo(1,0);
+        lista[1][0] = new NumeroComplejo(0,1);
+        lista[1][1] = new NumeroComplejo(1,0);
+        esperado[0][0] = new NumeroComplejo(1,0);
+        esperado[0][1] = new NumeroComplejo(0,1);
+        esperado[1][0] = new NumeroComplejo(1,0);
+        esperado[1][1] = new NumeroComplejo(1,0);
+        NumeroComplejo[][] respuesta = operador.Transpuesta(lista) ;
+        assertEquals(respuesta,esperado);
+    }
+     @Test
+     public void MatrizConjugada(){
+        
+        NumeroComplejo[][] lista = new NumeroComplejo[2][2];
+        NumeroComplejo[][]  esperado = new NumeroComplejo[2][2];
+        lista[0][0] =new NumeroComplejo(1,1);
+        lista[0][1] =new NumeroComplejo(1,1);
+        lista[1][0] = new NumeroComplejo(1,1);
+        lista[1][1] = new NumeroComplejo(1,1);
+        esperado[0][0] = new NumeroComplejo(-1,-1);
+        esperado[0][1] = new NumeroComplejo(-1,-1);
+        esperado[1][0] = new NumeroComplejo(-1,-1);
+        esperado[1][1] = new NumeroComplejo(-1,-1);
+        NumeroComplejo[][] respuesta = operador.inversaMatriz(lista) ;
+        assertEquals(respuesta,esperado);
+    }
+      @Test
+     public void MatrizAdjunta(){
+        
+         NumeroComplejo[][] lista = new NumeroComplejo[2][2];
+        NumeroComplejo[][]  esperado = new NumeroComplejo[2][2];
+        lista[0][0] =new NumeroComplejo(1,0);
+        lista[0][1] =new NumeroComplejo(1,0);
+        lista[1][0] = new NumeroComplejo(0,1);
+        lista[1][1] = new NumeroComplejo(1,0);
+        esperado[0][0] = new NumeroComplejo(1,0);
+        esperado[0][1] = new NumeroComplejo(0,-1);
+        esperado[1][0] = new NumeroComplejo(1,0);
+        esperado[1][1] = new NumeroComplejo(1,0);
+        NumeroComplejo[][] respuesta = operador.MatrizAdjunta(lista) ;
+        assertEquals(respuesta,esperado);
+    }
+      @Test
+      public void NormaMatriz() throws Exception{
+        NumeroComplejo[][] lista = new NumeroComplejo[2][2];
+        lista[0][0] =new NumeroComplejo(3,0);
+        lista[0][1] =new NumeroComplejo(5,0);
+        lista[1][0] = new NumeroComplejo(2,0);
+        lista[1][1] = new NumeroComplejo(3,0);
+        double esperado= Math.sqrt(47);
+        double respuesta = operador.NormaMatriz(lista) ;
+        assertEquals(respuesta,respuesta,0.7);
+    }
+       @Test
+    public void DistanciaMatriz(){
+        double DELTA = 1e-15;
+        double[][] lista = new double[2][2];
+        double[][] listaDos = new double[2][2];
+        lista[0][0] =3;
+        lista[0][1] =5;
+        lista[1][0] = 2;
+        lista[1][1] = 3;
+        listaDos[0][0] = 1;
+        listaDos[0][1] = 6;
+        listaDos[1][0]=2;
+        listaDos[1][1]= 4;
+        double esperado= 2.449489742783178;
+        double respuesta = operador.DistacnciaMatrizes(lista, listaDos) ;
+        assertEquals(respuesta,esperado,DELTA);
+    }
+     @Test
+    public void MatrizUnitaria() throws Exception{
+        
+        NumeroComplejo[][] lista = new NumeroComplejo[2][2];
+        lista[0][0] =new NumeroComplejo(3,2);
+        lista[0][1] =new NumeroComplejo(9,5);
+        lista[1][0] = new NumeroComplejo(5,-6);
+        lista[1][1] = new NumeroComplejo(1,4);
+        boolean esperado= false;
+        boolean respuesta = operador.EsMatrizUnitaria(lista) ;
+        assertEquals(respuesta,esperado);
+    }
+     @Test
+    public void MatrizHermitiana() throws Exception{
+        
+        NumeroComplejo[][] lista = new NumeroComplejo[2][2];
+        lista[0][0] =new NumeroComplejo(1,8);
+        lista[0][1] =new NumeroComplejo(8,1);
+        lista[1][0] = new NumeroComplejo(6,-6);
+        lista[1][1] = new NumeroComplejo(-6,6);
+        boolean esperado= false;
+        boolean respuesta = operador.EsMatrizUnitaria(lista) ;
+        assertEquals(respuesta,esperado);
+    }
+    @Test
+    public void TensorMatriz(){
+        
+        NumeroComplejo[][] lista = new NumeroComplejo[2][2];
+        NumeroComplejo[][] listaDos = new NumeroComplejo[2][2];
+        NumeroComplejo[][]  esperado = new NumeroComplejo[2][2];
+        lista[0][0] =new NumeroComplejo(1,0);
+        lista[0][1] =new NumeroComplejo(0,1);
+        lista[1][0] = new NumeroComplejo(1,0);
+        lista[1][1] = new NumeroComplejo(0,1);
+        listaDos[0][0] = new NumeroComplejo(0,1);
+        listaDos[0][1] = new NumeroComplejo(1,0);
+        listaDos[1][0]= new NumeroComplejo(0,1);
+        listaDos[1][1]= new NumeroComplejo(1,0);
+        esperado[0][0] = new NumeroComplejo(1,1);
+        esperado[0][1] = new NumeroComplejo(1,1);
+        esperado[1][0] = new NumeroComplejo(1,1);
+        esperado[1][1] = new NumeroComplejo(1,1);
+        NumeroComplejo[][] respuesta = operador.sumaMatriz(lista, listaDos) ;
+        assertEquals(respuesta,esperado);
+    }
 }
